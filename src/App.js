150e92bfaee91a2e87b854631b22d6a9efbda4e8
Peem_rayu <91822674+Peerayukun@users.noreply.github.com>
2022-08-23 15:32:35 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import {dataContext, Form } from "./components/form";
-import LocalConnection from "./components/localConnect";
 import Table from "./components/table";
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
   return (
     <dataContext.Provider value={{data:all,func:setAll}}>
         <Form/> 
-        <LocalConnection/>
         <Table/>
     </dataContext.Provider>
   );
